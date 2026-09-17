e7af3a51df242a1f92a374514410ddc6785fbfe9
sarvasri0703-ux <sarvasri0703@gmail.com>
2026-09-17 14:52:10 +0530

    Day-arithmetic-operation

---
+++ day1.py
@@ -0,0 +1,13 @@
+Python 3.10.9 (tags/v3.10.9:1dd9be6, Dec  6 2022, 20:01:21) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========================================================== RESTART: C:/Users/student117/Desktop/day01.py ==========================================================
+Enter first number: 5
+Enter second number: 5
+Addition = 10
+Subtraction = 0
+Multiplication = 25
+Division = 1.0
+Floor Division = 1
+Modulus = 0
+Exponentiation = 3125
